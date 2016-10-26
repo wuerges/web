82d6bea619d10bfc4e81636336eb4c2c740fbb8f
Emilio Wuerges <wuerges@gmail.com>
2016-10-26 20:54:44 -0200

    app

--- myApp/src/model/model.ts
+++ myApp/src/model/model.ts
@@ -15,15 +15,62 @@ export class Item {
     this.quantity     = quantity;
     this.quantity_set = quantity_set;
   }
+
+  priceSh() {
+    return this.price * this.quantity 
+      * this.quantity_set / 100;
+  }
+  quantitySh() {
+    return this.quantity 
+      * this.quantity_set / 100;
+  }
+
+  priceRemSh() {
+    return (this.price * this.quantity) - 
+      (this.price * this.quantity 
+       * this.quantity_set / 100);
+  }
+
+  quantityRemSh() {
+    return this.quantity - 
+      (this.quantity 
+       * this.quantity_set / 100)
+  }
 }
 
 export class Friend {
   name  : string;
-  items : Map<string, Item>;
+  items : Array<Item>;
 
   constructor(a_name: string) {
     this.name = a_name;
+    this.items = [];
   }
+
+  total(): number {
+    var t = 0;
+    for (let i of this.items) {
+      t += i.price * i.quantity;
+    }
+    return t;
+  }
+
+  addItem(name, price, qty) {
+    for(let i of this.items) {
+      if (i.name == name 
+          && i.price == price) {
+        i.quantity += qty;
+        return;
+      }
+    }
+    console.log("tried to add item:" 
+                + name + " "
+                + price + " "
+                + qty);
+    this.items.push(
+      new Item(name, price, qty, 0));
+  }
+
 }
 
 export class AppState {
@@ -57,5 +104,53 @@ export class AppState {
     this.unassigned.push(
       new Item(name, price, qty, 0));
   }
+
+  splitEqually(i : Item) {
+    let qt = i.quantity / this.friends.length;
+
+    for (let f of this.friends) {
+      f.addItem(i.name, i.price, qt);
+    }
+    i.quantity = 0;
+
+    this.unassigned = this.unassigned.filter(i => i.quantity > 0);
+  }
+
+  transfer(i, f) {
+    let s = i.quantity * i.quantity_set / 100;
+    i.quantity_set = 0;
+    i.quantity = i.quantity - s;
+    if (s > 0) {
+      f.addItem(i.name, i.price, s);
+    }
+    console.log("tried to add item:" 
+                + i.name + " "
+                + i.price + " "
+                + i.qty);
+  }
+
+  groupItems(is : Array<Item> ) {
+    // TODO
+  }
+
+  sendItemToFriend(f) {
+    console.log("sent to: " +  f.name);
+
+    for(let i of this.unassigned) {
+      this.transfer(i, f);
+    }
+    this.unassigned = this.unassigned.filter(i => i.quantity > 0);
+    this.groupItems(this.unassigned);
+
+    for(let fi of this.friends) {
+      for(let i of fi.items) {
+        this.transfer(i, f);
+      }
+      fi.items = fi.items.filter(i => i.quantity > 0);
+      this.groupItems(fi.items);
+    }
+
+  }
+
 
 }
--- myApp/src/pages/about/about.ts
+++ myApp/src/pages/about/about.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
-
-export type Item = {name: string, price:number, quantity: number, qty_s: number};
-export type Person = {name: string, items: Array<Item>};
+import { AppState, Item } from '../../model/model';
 
 @Component({
   selector: 'page-about',
@@ -11,71 +9,20 @@ export type Person = {name: string, items: Array<Item>};
 })
 export class AboutPage {
 
-  friends : Array<Person>;
-  items : Array<Item>;
+  appState : AppState;
 
   constructor(public navCtrl: NavController, navParams: NavParams) {
-    this.friends = []
-    for (let f of navParams.data.friends) {
-      console.log("f.name: " +  f);
-      this.friends.push({name: f, items: []});
-    }
-    this.items = navParams.data.items;
-    this.items.push(
-      { name: "Tip"
-      , price: navParams.data.tip
-      , quantity: 1
-      , qty_s: 0 });
-  }
 
-  total(f): number {
-    var t = 0;
-    for (let i of f.items) {
-      t += i.price * i.quantity;
-    }
-    return t;
-  }
-
-  groupItems(is : Array<Item> ) {
-    // TODO
+    this.appState = navParams.data.appState;
+    this.appState.addItem(
+      "Tip", navParams.data.tip, 1);
   }
 
   splitEqually(i : Item) {
-    let qt = i.quantity / this.friends.length;
-
-    for (let f of this.friends) {
-      f.items.push({name: i.name, price: i.price, quantity: qt, qty_s: 0});
-    }
-    i.quantity = 0;
-    
-    this.items = this.items.filter(i => i.quantity > 0);
-  }
-
-  transfer(i, f) {
-    let s = i.quantity * i.qty_s / 100;
-    i.qty_s = 0;
-    i.quantity = i.quantity - s;
-    if (s > 0) {
-      f.items.push({name: i.name, price: i.price, quantity: s, qty_s: 0});
-    }
+    this.appState.splitEqually(i);
   }
 
   sendItemToFriend(f) {
-    //console.log("sent to: " +  fname);
-
-    for(let i of this.items) {
-      this.transfer(i, f);
-    }
-    this.items = this.items.filter(i => i.quantity > 0);
-    this.groupItems(this.items);
-
-    for(let fi of this.friends) {
-      for(let i of fi.items) {
-        this.transfer(i, f);
-      }
-      fi.items = fi.items.filter(i => i.quantity > 0);
-      this.groupItems(fi.items);
-    }
-
+    this.appState.sendItemToFriend(f);
   }
 }
